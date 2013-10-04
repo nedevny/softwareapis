e58036243eedfd0689e2ebda6766d44913fd8b26
Doncho Minkov <Doncho.Minkov@telerik.com>
2013-10-04 08:35:18 -0500

    Changes made by Doncho Minkov (Doncho.Minkov@telerik.com).

--- Cordova-Software-APIs/js/app/notifications.js
+++ Cordova-Software-APIs/js/app/notifications.js
@@ -1,32 +1,26 @@
 var app = app || {};
+
 (function(application) {    
-        var alert = navigator.notification.alert;
-        var confirm = navigator.notification.confirm;
-        var prompt = navigator.notification.prompt;
-        var beep = navigator.notification.beep;
-        var vibrate = navigator.notification.vibrate;
-    
-        application.notifications = {
-            init:function() {
-            },
-            close: function() {            
-            },
-            alert: function() {
-                navigator.notification.alert("Alert!");
-            },
-            confirm: function() {
-                navigator.notification.confirm("Alert!");
-            },
-            prompt:function() {
-                var result = navigator.notification.prompt("Are you sure?");
-                navigator.notification.alert(result);
-            },
-            beep: function() {
-                beep(3);
-            },
-            vibrate: function() {
-                navigator.notification.vibrate(2000);
-            }
-        };
-    }(app));
-});
+    application.notifications = {
+        init:function() {
+        },
+        close: function() {            
+        },
+        alert: function() {
+            navigator.notification.alert("Alert!");
+        },
+        confirm: function() {
+            navigator.notification.confirm("Alert!");
+        },
+        prompt:function() {
+            var result = navigator.notification.prompt("Are you sure?");
+            navigator.notification.alert(result);
+        },
+        beep: function() {
+            beep(3);
+        },
+        vibrate: function() {
+            navigator.notification.vibrate(2000);
+        }
+    };
+}(app));
--- Cordova-Software-APIs/js/index.js
+++ Cordova-Software-APIs/js/index.js
@@ -2,6 +2,6 @@
     document.addEventListener("deviceready", function() {
         var app = new kendo.mobile.Application(document.body);
         $("#devices-list").kendoMobileListView();    
-        $(".listview").kendoMobileListView();
+        //$(".listview").kendoMobileListView();
     }, false);
 }());
