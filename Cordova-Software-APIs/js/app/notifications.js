25a067a3897f02a8510ea96d3220226ad74e5a0f
Doncho Minkov <Doncho.Minkov@telerik.com>
2013-10-04 08:29:37 -0500

    Changes made by Doncho Minkov (Doncho.Minkov@telerik.com).

--- Cordova-Software-APIs/js/app/notifications.js
+++ Cordova-Software-APIs/js/app/notifications.js
@@ -1,13 +1,32 @@
 var app = app || {};
-
-(function(application){
-    application.notifications = {
-        init:function(){
-            alert("initialized");
-        },
-        close: function(){
-            alert("closed");            
-        }
-    };
+(function(application) {    
+        var alert = navigator.notification.alert;
+        var confirm = navigator.notification.confirm;
+        var prompt = navigator.notification.prompt;
+        var beep = navigator.notification.beep;
+        var vibrate = navigator.notification.vibrate;
     
-}(app));
+        application.notifications = {
+            init:function() {
+            },
+            close: function() {            
+            },
+            alert: function() {
+                navigator.notification.alert("Alert!");
+            },
+            confirm: function() {
+                navigator.notification.confirm("Alert!");
+            },
+            prompt:function() {
+                var result = navigator.notification.prompt("Are you sure?");
+                navigator.notification.alert(result);
+            },
+            beep: function() {
+                beep(3);
+            },
+            vibrate: function() {
+                navigator.notification.vibrate(2000);
+            }
+        };
+    }(app));
+});
--- Cordova-Software-APIs/js/index.js
+++ Cordova-Software-APIs/js/index.js
@@ -1,5 +1,7 @@
-$(function() {
-    var app = new kendo.mobile.Application(document.body);
-    $("#devices-list").kendoMobileListView();
-    
-});
+(function() {
+    document.addEventListener("deviceready", function() {
+        var app = new kendo.mobile.Application(document.body);
+        $("#devices-list").kendoMobileListView();    
+        $(".listview").kendoMobileListView();
+    }, false);
+}());
